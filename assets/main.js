1ccf29410e69216a436ce6051f2bd13d17bdcff5
SalvatoreBrancato <salvatorebrancato90@gmail.com>
2023-03-24 15:26:18 +0100

    Bonus_1_completato

--- assets/main.js
+++ assets/main.js
@@ -1,19 +1,32 @@
 for (let numero = 1; numero <= 100; numero++) {
     
     if (numero % 3 == 0 && numero % 5 == 0) {
-        console.log("fizzbuzz");
+        const div = document.querySelector(`div.prova`);
+        const p = document.createElement('p');
+        p.append(`fizzbuzz`);
+        div.append(p);
+
     }
     
     else if (numero % 3 == 0) {
-        console.log("fizz");
+        const div = document.querySelector(`div.prova`);
+        const p = document.createElement('p');
+        p.append(`fizz`);
+        div.append(p);
     }
 
     else if(numero % 5 == 0) {
-        console.log("buzz");
+        const div = document.querySelector(`div.prova`);
+        const p = document.createElement('p');
+        p.append(`buzz`);
+        div.append(p);
     }
 
     else {
-        console.log(numero);
+        const div = document.querySelector(`div.prova`);
+        const p = document.createElement('p');
+        p.append(`${numero}`);
+        div.append(p);
     }
 }
 
