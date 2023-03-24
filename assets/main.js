570d3fc110778a2eed6c4d0f974eb678f6dab37f
SalvatoreBrancato <salvatorebrancato90@gmail.com>
2023-03-24 15:12:04 +0100

    Struttura_base_completata

---
+++ assets/main.js
@@ -0,0 +1,19 @@
+for (let numero = 1; numero <= 100; numero++) {
+    
+    if (numero % 3 == 0 && numero % 5 == 0) {
+        console.log("fizzbuzz");
+    }
+    
+    else if (numero % 3 == 0) {
+        console.log("fizz");
+    }
+
+    else if(numero % 5 == 0) {
+        console.log("buzz");
+    }
+
+    else {
+        console.log(numero);
+    }
+}
+
